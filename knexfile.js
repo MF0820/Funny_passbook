da227935cd15180f499d131c0a0013e3f7e1cb01
MF0820 <masafumi_fukushima_aa@mail.toyota.co.jp>
2023-06-08 14:36:07 +0900

    デプロイ４

--- knexfile.js
+++ knexfile.js
@@ -17,8 +17,7 @@ module.exports = {
     client: "pg",
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: "./db/migrations",
+      tableName: "knex_migrations",
     },
-    seeds: { directory: "./db/seeds" },
   },
 };
